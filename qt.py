9d5960a774c32d55c11fc1c5e4a8303e7f4f8b06
superdun <475098936@qq.com>
2016-11-28 09:41:06 +0800

    fix

--- qt.py
+++ qt.py
@@ -1,16 +1,28 @@
 import sys
 from PyQt4 import QtCore, QtGui, uic
 
-qtCreatorFile = "mainwindow.ui" # Enter file here.
- 
+qtCreatorFile = "mainwindow.ui"  # Enter file here.
+
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
- 
+
+
 class MyApp(QtGui.QMainWindow, Ui_MainWindow):
+
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
- 
+
+        self.confirm_n.clicked.connect(self.inputN)
+
+        
+
+
+    def inputN(self):
+        print '!!!!!'
+        
+
+
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     window = MyApp()
