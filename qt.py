fa179921b8f2af0e40cdee0a3e296ff8b70bf326
superdun <475098936@qq.com>
2016-11-29 20:22:06 +0800

    fix

--- qt.py
+++ qt.py
@@ -1,9 +1,11 @@
 import sys
 from PyQt4 import QtCore, QtGui, uic
+from PyQt4.QtGui import *
 
 qtCreatorFile = "mainwindow.ui"  # Enter file here.
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
 
 
 class MyApp(QtGui.QMainWindow, Ui_MainWindow):
@@ -19,9 +21,14 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
 
 
     def inputN(self):
-        print '!!!!!'
-        
+        n =  self.n.value()
 
+        YOlayout =QVBoxLayout(self.Y0Area)
+
+        for i in range(n):
+            inputY0 = QDoubleSpinBox(self.Y0Area)
+            YOlayout.addWidget(inputY0)
+        self.Y0Area.setLayout(YOlayout)
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
